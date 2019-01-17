b298dda66de4ce41925f45d4604ae509dd62c026
FanKaiqiang <fankaiqiang001@gmail.com>
2019-01-18 00:15:50 +0800

    让menu菜单也支持语言选取功能

--- background.js
+++ background.js
@@ -7,7 +7,12 @@ chrome.runtime.onInstalled.addListener(function () {
 })
 
 chrome.contextMenus.onClicked.addListener(function (info) {//点击菜单监听
-  if (info.menuItemId === 'translateMenu') {//跳转到翻译界面
-    chrome.tabs.create({ url: `https://translate.google.cn/#view=home&op=translate&sl=auto&tl=auto&text=${info.selectionText}` })
-  }
+  chrome.storage.sync.get(['sl', 'tl'], (result) => {//获取storage中语言设定的值
+      slValue = result.sl ? result.sl.value :'auto'
+      tlValue = result.sl ? result.tl.value :'auto'
+    if (info.menuItemId === 'translateMenu') {//跳转到翻译界面
+      chrome.tabs.create({ url: `https://translate.google.cn/#view=home&op=translate&sl=${slValue}&tl=${tlValue}&text=${info.selectionText}` })
+    }
+  })
+
 })
